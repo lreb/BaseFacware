428cd93523ee017ae15cd10e2519078d626d75b8
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2021-09-10 22:11:35 -0600

    added common ts to angular

--- Angular/BaseFacwareAngular/src/environments/environment.ts
+++ Angular/BaseFacwareAngular/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  services: {
+    NetBaseFacware: {
+      host:"https://localhost:44336"
+    }
+  }
 };
 
 /*
